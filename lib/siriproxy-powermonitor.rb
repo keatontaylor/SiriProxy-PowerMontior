670187aeeb9ac24a3bf058a53cb78d8c9bc27760
Keaton Taylor <keaton@geekologist.net>
2012-09-26 16:50:07 -0500

    Added new commands.

--- lib/siriproxy-powermonitor.rb
+++ lib/siriproxy-powermonitor.rb
@@ -12,6 +12,7 @@ class SiriProxy::Plugin::PowerMonitor < SiriProxy::Plugin
   listen_for(/power.*status/i) { show_power_usage }
   listen_for(/power.*usage/i) { show_power_usage }
   listen_for(/current.*power usage/i) { show_power_usage }
+  listen_for(/detailed.*power usage/i) { detailed_power_usage }
   
   def show_power_usage
     say "Checking the current power usage."
@@ -29,4 +30,21 @@ class SiriProxy::Plugin::PowerMonitor < SiriProxy::Plugin
       request_completed #always complete your request! Otherwise the phone will "spin" at the user!
     }
   end
+  
+  def detailed_power_usage
+     say "Checking the current power usage."
+
+     Thread.new {
+       page = HTTParty.get("http://#{self.host}/").body rescue nil
+       status = JSON.parse(page) rescue nil
+
+       if status
+         say "#{status["totalwatts"]}  total watts, #{status["leg0"]} from leg zero, #{status["leg1"]} from leg one."      
+       else
+         say "Sorry, I could not connect to the power monitoring system."
+       end
+
+       request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+     }
+   end
 end
