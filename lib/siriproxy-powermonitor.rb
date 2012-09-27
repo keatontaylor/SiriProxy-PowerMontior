82443b38f08ee708227c1671fc67181fe272bf57
Keaton Taylor <keaton@geekologist.net>
2012-09-27 14:39:46 -0500

    Cleaned up the code. Modified the behivor.

--- lib/siriproxy-powermonitor.rb
+++ lib/siriproxy-powermonitor.rb
@@ -8,55 +8,31 @@ class SiriProxy::Plugin::PowerMonitor < SiriProxy::Plugin
     self.host = config["host"]
   end
 
-  #capture thermostat status
-  listen_for(/.*detailed power usage/i) { detailed_power_usage }
+  #capture power status
   listen_for(/power.*status/i) { show_power_usage }
-  listen_for(/.*power usage/i) { show_power_usage }
-  listen_for(/current.*power usage/i) { show_power_usage }
-  listen_for(/detailed.*power usage/i) { detailed_power_usage }
+  listen_for(/power.*usage/i) { show_power_usage }
   
   
   def show_power_usage
-    say "Checking the current power usage."
-      page = HTTParty.get("http://#{self.host}/").body rescue nil
-      status = JSON.parse(page) rescue nil
+    say "Checking."
+    page = HTTParty.get("http://#{self.host}/").body rescue nil
+    status = JSON.parse(page) rescue nil
     
-      powerkW = status['totalwatts'].to_f() / 1000
-      if status
-        say "#{ powerkW.round(2).to_s() } kW are currently in use."
-        response = ask "Would you like a more detailed report?"
-        if(response =~ /.*yes/i) #process their response
-            leg0kW = status["leg0"].to_f() / 1000
-            leg1kW = status["leg1"].to_f() / 1000
-            say "#{leg0kW.round(2).to_s()} from leg zero and #{leg1kW.round(2).to_s()} from leg one.", spoken: "Here is your report."
-        end    
+    # convert the power to kW. 
+    powerkW = status['totalwatts'].to_f() / 1000
+    leg0kW = status["leg0"].to_f() / 1000
+    leg1kW = status["leg1"].to_f() / 1000
+    
+    if status
+      response = ask "#{ powerkW.round(2).to_s() } kW are currently in use. Would you like a full report?"
+      if (response =~ /.*yes/i)
+        say "#{ powerkW.round(2).to_s() } total. /n #{leg0kW.round(2).to_s()} from leg zero /n #{leg1kW.round(2).to_s()} from leg one", spoken: "Here is your report."
       else
-        say "Sorry, I could not connect to the power monitoring system."
+        say "Okay."
       end
-        
-      request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-  end
+    end
+    
+    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   
-  def detailed_power_usage
-     say "Getting a detailed report of power usage."
-
-     Thread.new {
-       page = HTTParty.get("http://#{self.host}/").body rescue nil
-       status = JSON.parse(page) rescue nil
-       
-       totalkW = status['totalwatts'].to_f() / 1000
-       leg0kW = status["leg0"].to_f() / 1000
-       leg1kW = status["leg1"].to_f() / 1000
-
-       if status
-         say "#{totalkW.round(2).to_s()} kW total." 
-         say "#{leg0kW.round(2).to_s()} from leg zero."
-         say "#{leg1kW.round(2).to_s()} from leg one."      
-       else
-         say "Sorry, I could not connect to the power monitoring system."
-       end
-
-       request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-     }
-   end
+  end
 end
